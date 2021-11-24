46a38ce879530e3fb423b8a3672b9adbcd0f0329
fengcunguang <fliangliang599@163.com>
2021-11-24 21:07:55 +0800

    add command

---
+++ doolall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./dool -a  -i -r -s  --aio --fs --ipc --lock --raw --socket --tcp --udp --unix --vm --vm-adv --zones --postgresql-backup --postgresql-dbsize --postgresql-log --postgresql-conn --postgresql-locks --postgresql-transactions --postgresql-buffer --output metrics.csv
