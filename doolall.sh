f3b47e9e44a4aa93ed15665d4ef3e04128046280
fengcunguang <fliangliang599@163.com>
2021-11-30 00:46:06 +0800

    add monitoring command

--- doolall.sh
+++ doolall.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+过去的采集数据：
+
+'''sh
 ./dool -a  -i -r -s  --aio --fs --ipc --lock --raw --socket --tcp --udp --unix --vm --vm-adv --zones --postgresql-backup --postgresql-dbsize --postgresql-log --postgresql-conn --postgresql-locks --postgresql-transactions --postgresql-buffer --output metrics.csv
+'''
+
+新的采集数据：
+
+需要先安装pg_statsinfo
+
+'''sh
+./dool1 --postgresql-time -a -s -i -r -y --swap --aio --fs --ipc --lock --raw --socket --tcp --udp --unix --vm --vm-adv --zones --postgresql-dbsize --postgresql-conn --postgresql-lockwaits --postgresql-transactions --postgresql-buffer --postgresql-settings --output ./metrics1.csv
+
+./dool2 --postgresql-sql-total-time --postgresql-sql-mean-time --postgresql-sql-calls --postgresql-sql-io-time --output2 ./metrics2.csv --postgresql-table-n-dml --postgresql-table-expand-rate --postgresql-table-pyhsical-read --postgresql-table-seq-read --output3 ./metrics3.csv
+'''
