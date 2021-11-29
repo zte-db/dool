9e5c4551f38479872bba68a909c1d8ee6684cdfe
fengcunguang <fliangliang599@163.com>
2021-11-30 00:32:24 +0800

    merge code from zte dba

---
+++ plugins/dool_postgresql_sql_calls.py
@@ -0,0 +1,165 @@
+global pg_user
+pg_user = os.getenv('DSTAT_PG_USER') or os.getenv('USER')
+
+global pg_pwd
+pg_pwd = os.getenv('DSTAT_PG_PWD')
+
+global pg_host
+pg_host = os.getenv('DSTAT_PG_HOST')
+
+global pg_port
+pg_port = os.getenv('DSTAT_PG_PORT')
+
+
+class dstat_plugin(dstat):
+    """
+    Plugin for PostgreSQL connections.
+    """
+
+    def __init__(self):
+        self.name = 'postgresql sql_calls'
+        self.nick = ('start_time','end_time','io_time','rolname','dbname','calls','total_time','mean_time','blk_read_time','blk_write_time','queryid','query', )
+        self.vars = self.nick
+        self.type = 'f'
+        self.width = 5
+        self.scale = 1
+
+    def check(self):
+        global psycopg2
+        import psycopg2
+        try:
+            args = {}
+            if pg_user:
+                args['user'] = pg_user
+            if pg_pwd:
+                args['password'] = pg_pwd
+            if pg_host:
+                args['host'] = pg_host
+            if pg_port:
+                args['port'] = pg_port
+
+            self.db = psycopg2.connect(**args)
+        except Exception as e:
+            raise Exception('Cannot interface with PostgreSQL server, %s' % e)
+
+    def write_file(self,file_name,data):
+        with open(file_name, 'a+') as fp:
+            fp.write(data + '\n')
+
+    def extract(self,outputfile):
+        try:
+            c = self.db.cursor()
+            sql='''select snapid from statsrepo.snapshot where time>now()-interval '20 mins' order by snapid desc limit 2;'''
+            c.execute(sql)
+            out = c.fetchall()
+            snapid = [out[1][0], out[0][0]]
+            sql = '''
+            SELECT
+                ceil(extract(epoch from start_time)) as start_time,
+                ceil(extract(epoch from end_time)) as end_time,
+                ((t1.blk_read_time + t1.blk_write_time) / t1.calls)::numeric(1000, 3) AS io_time,
+                t1.rolname::text,
+                t1.dbname::name,
+                t1.calls,
+                t1.total_time::numeric(1000, 3),
+                (t1.total_time / t1.calls)::numeric(1000, 3) as mean_time,
+                t1.blk_read_time::numeric(1000, 3),
+                t1.blk_write_time::numeric(1000, 3),
+                t1.queryid,
+                substr(t1.query,1,2000) as query
+            FROM
+                (SELECT
+                    st1.start_time,
+                    st2.end_time,
+                    rol.name AS rolname,
+                    db.name AS dbname,
+                    st2.query,
+                    st2.queryid,
+                    statsrepo.sub(st2.calls, st1.calls) AS calls,
+                    statsrepo.sub(st2.total_time, st1.total_time) AS total_time,
+                    statsrepo.sub(st2.blk_read_time, st1.blk_read_time) AS blk_read_time,
+                    statsrepo.sub(st2.blk_write_time, st1.blk_write_time) AS blk_write_time
+                FROM
+                    (SELECT
+                        s.snapid,
+                        s.dbid,
+                        s.userid,
+                        s.queryid,
+                        s.query,
+                        s.calls,
+                        s.total_time,
+                        s.blk_read_time,
+                        s.blk_write_time,
+                ss.time as end_time
+                    FROM
+                        statsrepo.statement s
+                        JOIN statsrepo.snapshot ss ON (ss.snapid = s.snapid)
+                    WHERE
+                        s.snapid = %s 
+                        AND ss.instid = (SELECT instid FROM statsrepo.snapshot ss1 WHERE ss1.snapid = %s)
+                    ) AS st2
+                    LEFT JOIN (SELECT
+                        s.snapid,
+                        s.dbid,
+                        s.userid,
+                        s.queryid,
+                        s.query,
+                        s.calls,
+                        s.total_time,
+                        s.blk_read_time,
+                        s.blk_write_time,
+                        ss.time as start_time
+                    FROM
+                        statsrepo.statement s
+                        JOIN statsrepo.snapshot ss ON (ss.snapid = s.snapid)
+                    WHERE
+                        s.snapid = %s 
+                        AND ss.instid = (SELECT instid FROM statsrepo.snapshot ss1 WHERE ss1.snapid = %s)
+                    ) AS st1 ON 
+                        (st1.dbid = st2.dbid AND st1.userid = st2.userid AND
+                        st1.queryid = st2.queryid )
+                    JOIN statsrepo.database db ON
+                        (db.snapid = st2.snapid AND db.dbid = st2.dbid)
+                    JOIN statsrepo.role rol ON
+                        (rol.snapid = st2.snapid AND rol.userid = st2.userid)
+                ) AS t1
+            WHERE t1.calls > 0 and t1.total_time > 0
+            ORDER BY
+                6 DESC
+                limit 30;
+            ''' % (snapid[1], snapid[1], snapid[0], snapid[1])
+            c.execute(sql)
+            # io_time,rolname,dbname,calls,total_time,mean_time,blk_read_time,blk_write_time,queryid,query = c.fetchall()
+            output = c.fetchall()
+
+            line1 = '-----------------------------------------------------------------------------postgresql-sql_calls----------------------------------------------------------------------------'
+            line2 = 'start_time      end_time        io_time         rolname          dbname          calls         total_time      mean_time     blk_read_time   blk_write_time      queryid      query     '
+            self.write_file(outputfile,'\n')
+            self.write_file(outputfile,line1)
+            self.write_file(outputfile,line2)
+
+            if output == []:
+                for name in self.vars:
+                    self.val[name] = -1
+            else:
+                for a in output:
+                    aa = ''
+                    for p in a:
+                        t = str(p)
+                        aa += t.replace('\n',' ').replace('\t',' ') + ','
+                    self.write_file(outputfile, aa)
+
+            length = len(output)
+            if length < 30 and length >= 1:
+                hh = output[0]
+                start_time = str(hh[0])
+                end_time = str(hh[1])
+                shuchu = start_time + ',' + end_time + ',0,0,0,0,0,0,0,0,0,'
+                for i in range(length,30):
+                    self.write_file(outputfile, shuchu)   
+
+        except Exception as e:
+            for name in self.vars:
+                self.val[name] = -1
+
+# vim:ts=4:sw=4:et
---
+++ plugins/dool_postgresql_table_seq_read.py
@@ -0,0 +1,236 @@
+global pg_user
+pg_user = os.getenv('DSTAT_PG_USER') or os.getenv('USER')
+
+global pg_pwd
+pg_pwd = os.getenv('DSTAT_PG_PWD')
+
+global pg_host
+pg_host = os.getenv('DSTAT_PG_HOST')
+
+global pg_port
+pg_port = os.getenv('DSTAT_PG_PORT')
+
+global pg_database
+pg_database = os.getenv('DSTAT_PG_DATABASE')
+
+class dstat_plugin(dstat):
+    """
+    Plugin for PostgreSQL seq_read.
+    """
+
+    def __init__(self):
+        self.name = 'postgresql table_seq_read'
+        self.nick = ('time', 'db_name', 'relid', 'schemaname', 'relname','seq_scan','seq_tup_read','idx_scan',
+                    'idx_tup_fetch','heap_blks_read','heap_blks_hit','n_tup_dml','relpages','n_indexes','expand_rate')
+        self.vars = ['start_time','end_time', 'db_name', 'relid', 'schemaname', 'relname', 'seq_scan','seq_tup_read',
+                    'idx_scan', 'idx_tup_fetch', 'heap_blks_read', 'heap_blks_hit', 'total_read','physical_read_rate',
+                    'n_tup_dml','relpages_change','relpages', 'n_indexes', 'expand_rate_change', 'expand_rate']
+        self.type = 'f'
+        self.width = 5
+        self.scale = 1
+        self.last_out = []
+
+    def check(self):
+        global psycopg2
+        import psycopg2
+        try:
+            args = {}
+            if pg_user:
+                args['user'] = pg_user
+            if pg_pwd:
+                args['password'] = pg_pwd
+            if pg_host:
+                args['host'] = pg_host
+            if pg_port:
+                args['port'] = pg_port
+            if pg_database:
+                args['database'] = pg_database
+            
+            self.db = psycopg2.connect(**args)
+        except Exception as e:
+            raise Exception('Cannot interface with PostgreSQL server, %s' % e)
+
+    def write_file(self, file_name, data):
+        with open(file_name, 'a+') as fp:
+            fp.write(data + '\n')
+
+    def sort_result(self, key, result):
+        count = 0
+        for i in self.vars:
+            if i == key:
+                break
+            count += 1
+        result_ = []
+        for i in range(0,len(result['relid'])):
+            tmp = []
+            for key in self.vars:
+                tmp.append(result[key][i])
+            result_.append(tmp)
+        result_.sort(key=lambda x:x[count], reverse=True)
+        res = self.completion_result(result_)
+        return res
+
+    def completion_result(self, result):
+        hh = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        length = len(result)
+        if length < 50:
+            delta = 50 - length
+            for i in range(0, delta):
+                result.append(hh)
+            return result
+        else:
+            res = []
+            for i in range(0,50):
+                res.append(result[i])       
+            return res
+
+    def extract(self, outputfile):
+        try:
+            import os,sys
+            except_line = ['time', 'db_name', 'relid', 'schemaname', 'relpages',
+                           'relname', 'n_indexes','expand_rate']
+            c = self.db.cursor()
+            sql = '''
+            select ceil(extract(epoch from now())) as time,current_database() as db_name,t.relid,t.schemaname,t.relname,s.seq_scan,s.seq_tup_read,s.idx_scan,s.idx_tup_fetch,t.heap_blks_read,t.heap_blks_hit,(s.n_tup_ins+s.n_tup_upd+s.n_tup_del) as n_tup_dml,c.relpages,d.n_indexes,
+                (case when c.relpages>1 then (100-c.reltuples::bigint*(i.stawidth::bigint+26)*100/(c.relpages::bigint*8*1024)) else 0 end) as expand_rate
+                from 
+                pg_class c,pg_statio_all_tables t ,pg_stat_all_tables s ,(select starelid,sum(stawidth) as stawidth from pg_statistic group by starelid) as i,(select indrelid,count(1) as n_indexes from pg_index group by indrelid) as d
+                where 
+                c.oid=s.relid 
+                and c.oid=t.relid
+                and c.oid=i.starelid
+                and c.oid=d.indrelid 
+                and t.schemaname not in ('statsrepo','statsinfo','pg_catalog','pg_toast','information_schema') 
+                order by relid asc;
+            '''
+            c.execute(sql)
+            output = c.fetchall()
+            out_dict = {
+                'time':[],
+                'db_name':[], 
+                'relid':[], 
+                'schemaname':[], 
+                'relname':[],
+                'seq_scan':[],
+                'seq_tup_read':[],
+                'idx_scan':[],
+                'idx_tup_fetch':[],
+                'heap_blks_read':[],
+                'heap_blks_hit':[],
+                'n_tup_dml':[],
+                'relpages':[],
+                'n_indexes':[],
+                'expand_rate':[]
+            }
+
+            result = {
+                'start_time':[],
+                'end_time':[],
+                'db_name':[], 
+                'relid':[], 
+                'schemaname':[], 
+                'relname':[],
+                'seq_scan':[],
+                'seq_tup_read':[],
+                'idx_scan':[],
+                'idx_tup_fetch':[],
+                'heap_blks_read':[],
+                'heap_blks_hit':[],
+                'total_read':[],
+                'physical_read_rate':[],
+                'n_tup_dml':[],
+                'relpages_change':[],
+                'relpages':[],
+                'n_indexes':[],
+                'expand_rate_change':[],
+                'expand_rate':[]
+            }
+
+            result_list = ['time', 'db_name', 'relid', 'schemaname', 'relname', 'seq_scan','seq_tup_read',
+                           'idx_scan', 'idx_tup_fetch', 'heap_blks_read', 'heap_blks_hit', 'n_tup_dml',
+                           'relpages', 'n_indexes', 'expand_rate']
+
+
+            for i in output:
+                for count in range(0, len(self.nick)):
+                    if count == 0:
+                        time = str(i[count])
+                        out_dict[self.nick[count]].append(time)
+                    else:
+                        out_dict[self.nick[count]].append(i[count])
+
+            if self.last_out == []:
+                pass
+            else:
+                last_output = self.last_out
+
+                for i in range(0,len(out_dict['relid'])):
+                    for key in except_line:
+                        if key != 'time':
+                            result[key].append(out_dict[key][i])
+
+                    if out_dict['relid'][i] not in last_output['relid']:
+                        # new record
+                        for key in result_list:
+                            if key in except_line:
+                                pass
+                            else:
+                                result[key].append(out_dict[key][i])
+                        result['start_time'].append(out_dict['time'][i])
+                        result['end_time'].append(out_dict['time'][i])
+                        result['expand_rate_change'].append(0)
+                        result['relpages'].append(0)
+                    else:
+                        # old record
+                        last_count = 0
+                        for key in last_output['relid']:
+                            if out_dict['relid'][i] == key:
+                                break
+                            last_count += 1
+                        result['start_time'].append(last_output['time'][last_count])
+                        result['end_time'].append(out_dict['time'][i])
+                        for key in result_list:
+                            if key in except_line:
+                                pass
+                            else:
+                                res = out_dict[key][i] - last_output[key][last_count]
+                                result[key].append(res)
+                        result['expand_rate_change'].append(out_dict['expand_rate'][i] - last_output['expand_rate'][last_count])
+                        result['relpages_change'].append(out_dict['relpages'][i] - last_output['relpages'][last_count])
+                        
+                    blks = out_dict['heap_blks_read'][i] + out_dict['heap_blks_hit'][i]
+                    result['total_read'].append(blks)
+                    if blks == 0:
+                        result['physical_read_rate'].append(0)
+                    else:
+                        result['physical_read_rate'].append(out_dict['heap_blks_read'][i] * 100 / blks)
+
+                seq_tup_read_ = self.sort_result('seq_tup_read', result)
+
+                line1 = '-----------------------------------------------------------------------------postgresql-table_seq_read----------------------------------------------------------------------------'
+                line2 = ''
+                for i in self.vars:
+                    line2 += i + '  '
+                self.write_file(outputfile,'\n')
+                self.write_file(outputfile,line1)
+                self.write_file(outputfile,line2)
+                for i in seq_tup_read_:
+                    t = ''
+                    c = 0
+                    for j in i:
+                        if c != len(i) - 1:
+                            t += str(j) + ', '
+                        else:
+                            t += str(j)
+                        c += 1
+                    self.write_file(outputfile,t)
+
+            self.last_out = out_dict
+
+        except Exception as e:
+            print(e)
+            self.last_out = []
+            for name in self.vars:
+                self.val[name] = -1
+
+# vim:ts=4:sw=4:et
